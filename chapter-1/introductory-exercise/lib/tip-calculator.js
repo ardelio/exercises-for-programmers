9ae89473d03bbd353a9a67486dd3cd69d03d7d5b
Anthony Sceresini <asceresini@gmail.com>
2015-10-22 18:13:25 +1100

    finished solution. Test driven inputs.

--- chapter-1/introductory-exercise/lib/tip-calculator.js
+++ chapter-1/introductory-exercise/lib/tip-calculator.js
@@ -2,12 +2,15 @@
 
 class TipCalculator {
   constructor (billAmount, tipRate) {
+    validateBillAmount(billAmount)
+    validateTip(tipRate)
     this.billAmount = billAmount
     this.tipRate = tipRate
   }
 
   tip () {
-    return this.billAmount * this.tipRate / 100
+    let tipString = (this.billAmount * this.tipRate / 100).toFixed(2)
+    return parseFloat(tipString)
   }
 
   total () {
@@ -16,3 +19,20 @@ class TipCalculator {
 }
 
 export default TipCalculator
+
+let validateBillAmount = (billAmount) => {
+  let hasCents = /^\d+\.\d{2}$/
+  if( isNaN(Number(billAmount)) || Number(billAmount) < 0) {
+    throw new TypeError('Not a Number')
+  }
+
+  if(! Number.isInteger(billAmount) && ! hasCents.test(billAmount)) {
+    throw new RangeError('Bill amount is not a valid $ value. Either $ or $.cc')
+  }
+}
+
+let validateTip = (tip) => {
+  if(! Number.isInteger(tip) || Number(tip) < 0 ) {
+    throw new TypeError('Not a valid tip')
+  }
+}
--- chapter-1/introductory-exercise/test/tip-calculator.spec.js
+++ chapter-1/introductory-exercise/test/tip-calculator.spec.js
@@ -4,28 +4,74 @@ import TipCalculator from '../lib/tip-calculator'
 import should from 'should'
 
 describe('TipCalculator', () => {
+  describe('on init with incorrect parameters', () => {
+    [
+      {
+        inputs: { billAmount: 10, tip: 0.15 },
+        outputs: { error: TypeError}
+      },
+      {
+        inputs: { billAmount: 10.001, tip: 15 },
+        outputs: { error: RangeError}
+      },
+      {
+        inputs: { billAmount: 'abcd', tip: 15 },
+        outputs: { error: TypeError}
+      },
+      {
+        inputs: { billAmount: 10, tip: 'abcd' },
+        outputs: { error: TypeError}
+      },
+      {
+        inputs: { billAmount: -10, tip: 15 },
+        outputs: { error: TypeError}
+      },
+      {
+        inputs: { billAmount: 10, tip: -15 },
+        outputs: { error: TypeError}
+      }
+
+    ].forEach( (subject) => {
+      let tipCalculator
+
+      describe(`with { billAmount: ${subject.inputs.billAmount}, tip: ${subject.inputs.tip}}`, () => {
+        it(`will raise a ${subject.outputs.error.name}`, () => {
+          (() => {
+            tipCalculator = new TipCalculator(subject.inputs.billAmount, subject.inputs.tip)
+          }).should.throw(subject.outputs.error)
+        })
+      })
+    })
+  });
   [
     {
-      inputs:  { billAmount: 10, tipAmount: 15 },
+      inputs:  { billAmount: 10, tip: 15 },
       outputs: { tip: 1.50, total: 11.50 }
+    },
+    {
+      inputs:  { billAmount: 11.25, tip: 15 },
+      outputs: { tip: 1.69, total: 12.94 }
     }
   ].forEach( (subject) => {
     let tipCalculator
 
     beforeEach(() => {
-      tipCalculator = new TipCalculator(subject.inputs.billAmount, subject.inputs.tipAmount);
+      tipCalculator = new TipCalculator(subject.inputs.billAmount, subject.inputs.tip)
     })
 
-    describe('#tip', () => {
-      it(`will return \$${subject.outputs.tip}`, () => {
-        tipCalculator.tip().should.eql(subject.outputs.tip)
+    describe(`with { billAmount: ${subject.inputs.billAmount}, tip: ${subject.inputs.tip}}`, () => {
+      describe('#tip', () => {
+        it(`will return \$${subject.outputs.tip}`, () => {
+          tipCalculator.tip().should.eql(subject.outputs.tip)
+        })
+      })
+
+      describe('#total', () => {
+        it(`will return \$${subject.outputs.total}`, () => {
+          tipCalculator.total().should.eql(subject.outputs.total)
+        })
       })
     })
 
-    describe('#total', () => {
-      it(`will return \$${subject.outputs.total}`, () => {
-        tipCalculator.total().should.eql(subject.outputs.total)
-      })
-    })
   })
 })
